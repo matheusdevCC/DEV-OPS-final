80155f91cf00cd166beaacd37ec301c2bc7807f6
Matheus Borges <matheus.borges@fcmsantacasasp.edu.br>
2025-11-25 21:08:29 -0300

    Ajuste job de perfomance

--- app.py
+++ app.py
@@ -1,15 +1,16 @@
-import os
 from flask import Flask, jsonify, request
-from flasgger import Swagger
 from flask_jwt_extended import JWTManager, create_access_token, jwt_required
+from flasgger import Swagger
+
 
 app = Flask(__name__)
 
-# JWT - agora usando variável de ambiente
-app.config['JWT_SECRET_KEY'] = os.getenv("JWT_SECRET_KEY", "local-default-secret")
+# JWT
+app.config['JWT_SECRET_KEY'] = 'your_secret_key'
 jwt = JWTManager(app)
 
-# Swagger config
+
+# Swagger
 swagger_template = {
     "swagger": "2.0",
     "info": {
@@ -21,9 +22,11 @@ swagger_template = {
 
 swagger = Swagger(app, template=swagger_template)
 
+
 @app.route('/')
 def home():
     return jsonify(message="API is running"), 200
+
 
 @app.route('/items', methods=['GET'])
 def get_items():
@@ -35,6 +38,7 @@ def get_items():
         description: Lista de itens
     """
     return jsonify(items=["item1", "item2", "item3"]), 200
+
 
 @app.route('/login', methods=['POST'])
 def login():
@@ -62,9 +66,11 @@ def login():
     token = create_access_token(identity="user")
     return jsonify(access_token=token), 200
 
+
 @app.route('/login', methods=['GET', 'PUT', 'DELETE'])
 def login_invalid_method():
     return jsonify(error="Method not allowed"), 405
+
 
 @app.route('/protected', methods=['GET'])
 @jwt_required()
@@ -77,7 +83,3 @@ def protected():
         description: OK
     """
     return jsonify(message="Protected route"), 200
-
-if __name__ == "__main__":
-    # debug para uso local
-    app.run(debug=True, host="0.0.0.0", port=5000)
